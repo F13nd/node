cd7010da213eb4b1f9130028ef7443ce4259ffce
tyo1997 <mtegarsetyo07@gmail.com>
2020-10-16 19:49:52 +0700

    hahahaa

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-//import default node js package
+
 const path = require("path");
 
-//import npm package
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
-//import controller
+
 const errorController = require("./controllers/error");
 
 const app = express();
 
-//set template engine template engine
-//ini komentar
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-//import route
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-//static dir
+
 app.use(express.static(path.join(__dirname, "public")));
 
-//route
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
